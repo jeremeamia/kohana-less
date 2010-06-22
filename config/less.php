28ee065a652fc60fa7d377e2d7d354d1d361dd7c
Jeremy Lindblom <jeremeamia@gmail.com>
2010-06-22 12:18:54 -0700

    reordered config items by importance.

--- config/less.php
+++ config/less.php
@@ -3,16 +3,16 @@
 return array
 (
 
-	// Minify the CSS before saving to the CSS file
-	'minify'    => FALSE,
+	// The path (starting from DOCROOT) to where the Less files are
+	'less_path' => 'assets/css/less',
 
 	// The path (starting from DOCROOT) to where the CSS files are
 	'css_path'  => 'assets/css',
 
-	// The path (starting from DOCROOT) to where the Less files are
-	'less_path' => 'assets/css/less',
-
 	// Lock the CSS file and do not attempt to recompile (Use in production)
 	'lock_css'  => FALSE,
+
+	// Minify the CSS before saving to the CSS file
+	'minify'    => FALSE,
 
 );
